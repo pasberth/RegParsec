d21970646bde9f965b563e43a0c54d1ac79fe49d
pasberth <pasberth@gmail.com>
2012-04-02 17:58:39 +0900

    Pass tests

--- spec/regparsers/string_parser_spec.rb
+++ spec/regparsers/string_parser_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe RegParsec::Regparsers::StringParser do
   
-  subject { described_class.new("aaa") }
+  subject { described_class.new.curry!("aaa") }
 
   example { subject.parse("aaa").should == "aaa" }
-  example { subject.regparse("aaa").should == ::RegParsec::Result::Success.new( :matching_string => "aaa" ) }
+  example { subject.regparse("aaa").should == ::RegParsec::Result::Success.new( :return_value => "aaa", :matching_string => "aaa" ) }
 end
